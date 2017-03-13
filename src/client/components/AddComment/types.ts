ef896aed57101a1bcc6c4feb8ee973279b3fe25c
Ryan Collins <rccproductions@mac.com>
2017-03-12 23:26:14 -0400

    Feat: delete unused components

--- src/client/components/AddComment/types.ts
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import AddComment from './';
-
-export interface AddCommentProps extends React.Props<typeof AddComment> {
-  input: string;
-  onSubmit: Function;
-  onChange: Function;
-  onKeyUp: Function;
-}
--- src/client/components/index.ts
+++ src/client/components/index.ts
@@ -1,15 +1,4 @@
 /* GENERATOR-IMPORT */
-import PostCard from './PostCard';
-import AddComment from './AddComment';
-import Comment from './Comment';
-import Post from './Post';
-import NavBar from './NavBar';
-
 export {
   /* GENERATOR-EXPORT */
-  PostCard,
-  AddComment,
-  Comment,
-  Post,
-  NavBar,
 };
